b21d9fb6baddc3eb18086664ddcbe48d274fea98
Hubert Jabłoński <jjjablosss@gmail.com>
2017-03-25 23:24:17 +0100

    Update XmassTree WIP.java

--- XmassTree WIP.java
+++ XmassTree WIP.java
@@ -1,10 +1,9 @@
-import org.apache.xmlbeans.xml.stream.Space;
 import sun.awt.Symbol;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class coscos {
+public class XmassTreeMain {
     public static void main(String[] args)
     {
         String Direction;
@@ -31,66 +30,66 @@ public class coscos {
             case "LEFT" :
                 XmassTreeLeft(Height, Character);
                 break;
+
+            default:
+                System.out.print("Wybrano nie poprawny kierunek.");
+                break;
         }
 
     }//Koniec Main
 
     public static int getHeight(){
-        int Wejsciowa;
+        int Wejsciowa = 0;
         System.out.print("Podaj Wysokosc choinki : ");
         for(;;) {
             Scanner HeightScanner = new Scanner(System.in);
             if(HeightScanner.hasNextInt()){
                 //  Wejsciowa.push(HeightScanner.nextInt());
                 Wejsciowa = HeightScanner.nextInt();
-                return Wejsciowa;
             }//Koniec petli if
+            if (Wejsciowa > 0){
+                return Wejsciowa;
+            }//Koniec petli if wew.
+            else if(Wejsciowa <= 0){
+                System.out.print("Podaj liczbe dodatnia wieksza od 0: ");
+            }//Koniec If wew.
             else{
                 System.out.print("Podaj poprawna wartosc wysokosci choinki : ");
-            }
+            }//Koniec Else Default
         }//Koniec Petli For
     }//Koniec klasy getHeight
 
     public static char getCharacter(){
-        char Znak, defaultCharacter;
-        String StringZnak;
+        char Znak =' ', defaultCharacter;
+        String StringZnak = " ";
         System.out.print("Podaj pojedynczy znak (np.*)  : ");
         Scanner scanCharacter = new Scanner(System.in);
         if(scanCharacter.hasNext()) {
             StringZnak = scanCharacter.nextLine();
             Znak = StringZnak.charAt(0);
-           if(StringZnak.length() == 1)
-            return Znak;
-            else
-                defaultCharacter = '*';
-                System.out.println("Podano nie poprawny znak, uzywanie znaku domyslnego : '" + defaultCharacter + "'");
         }//Koniec Petli if
-        else{
-            System.out.print("Podaj poprawny pojedyczny znak np.'*' : ");
-        }//Koniec Else
-        return '*';
+        if(StringZnak.length() == 1){
+            return Znak;
+        }
+        else
+            System.out.println("Podano znak dluzszy niz jeden znak. Uzycie domyslnego znaku.(*) ");
+            return '*';
+
     }//Koniec klasy getCharacter;
 
-    public enum DirectionDatabase { //Definiowanie prawidlowych odpowiedzi do getDirection()
-        UP, DOWN, LEFT, RIGHT;
-    }
 
     public static String getDirection(){
         String kierunek;
         String Tempname;
         System.out.print("Podaj kierunek w którym ma być narysowana choinka (UP,DOWN,RIGHT,LEFT) : ");
         for(;;) {
+
             Scanner DirectionScanner = new Scanner(System.in);
             kierunek = DirectionScanner.next();
             kierunek = kierunek.toUpperCase();
-            for(DirectionDatabase dir : DirectionDatabase.values()){
-                Tempname = dir.name();
-                if(Tempname.equals(kierunek)) {
-                    return kierunek;
-                }//Koniec petli if
-            }//Koniec Petli For
+
             return kierunek;
-           // System.out.print("Podaj kierunek w którym ma być narysowana choinka (UP,DOWN,RIGHT,LEFT) : ");
+
         }//Koniec petli for
     }//Koniec klasy getDirection
 
@@ -132,11 +131,11 @@ public class coscos {
                 if(RandomBubbles(1,10) % 4 == 0)
                     System.out.print("o");
                 else
-            System.out.print(Character);
+                    System.out.print(Character);
             }//Koniec petli if zew.
-                else
+            else
                 System.out.print(Character);
-            }//Koniec petli for;
+        }//Koniec petli for;
         if(!ifside)
             System.out.println("");
     }//Koniec funkcji drawCharacter
@@ -148,45 +147,45 @@ public class coscos {
 
     }//Koniec klasy RandomBubbles
 
-   public static void drawCharacterLUpRDown(boolean side, int Height, char Character){
+    public static void drawCharacterLUpRDown(boolean side, int Height, char Character){
 
-       for(int SymbolInt = 0; SymbolInt < Height - 1; SymbolInt++){
-           for(int SpaceInt = 0;SpaceInt < Height - SymbolInt - 1; SpaceInt++){
+        for(int SymbolInt = 0; SymbolInt < Height - 1; SymbolInt++){
+            for(int SpaceInt = 0;SpaceInt < Height - SymbolInt - 1; SpaceInt++){
                 if(!side)
-               System.out.print(" ");
+                    System.out.print(" ");
                 else
-                System.out.print(Character);
+                    System.out.print(Character);
 
-           }//Koneic petli for wew.
+            }//Koneic petli for wew.
 
-           for(int CharacterInt = 0; CharacterInt <= SymbolInt; CharacterInt++){
-               if(!side)
-               System.out.print(Character);
+            for(int CharacterInt = 0; CharacterInt <= SymbolInt; CharacterInt++){
+                if(!side)
+                    System.out.print(Character);
                 else
-               System.out.print(" ");
+                    System.out.print(" ");
 
-           }//Koniec petli for wew.
+            }//Koniec petli for wew.
 
-           System.out.println("");
+            System.out.println("");
 
-       }//Koniec petli for zew.
-   }//Koniec klasy drawCharacterLUpRDown
+        }//Koniec petli for zew.
+    }//Koniec klasy drawCharacterLUpRDown
 
     public static void drawCharacterRUpLDown(boolean side, int Height, char Character){
 
         for(int SymbolInt = Height -1; SymbolInt  > 0; SymbolInt--){
             for(int LineInt = Height - SymbolInt; LineInt > 0; LineInt--){
                 if(!side)
-                System.out.print(" ");
+                    System.out.print(" ");
                 else
-                System.out.print(Character);
+                    System.out.print(Character);
             }//Koniec petli for wew.
 
             for(int SpaceInt = 0; SpaceInt < SymbolInt; SpaceInt++) {
                 if(!side)
-                System.out.print(Character);
+                    System.out.print(Character);
                 else
-                System.out.print(" ");
+                    System.out.print(" ");
             }//Koniec Petli for wew.
 
             System.out.println("");
